e96e04941437a8b0f59fb70ad599f787c2647b00
clifford <clifford@noukod.com>
2019-02-21 18:36:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const currentUser = "Clifford LOUIS";
 
 const welcomeMessage = `Welcome to Flatbook,  ${currentUser}!`;
 
-const excitedWelcomeMessage = 'Welcome to Flatbook,${currentUser}.uppercase!'
+const excitedWelcomeMessage = 'Welcome to Flatbook,$ currentUser.toUpperCase()!'
