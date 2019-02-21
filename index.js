4cd62c82317f27b5138961c6acade58fda0bd0f0
clifford <clifford@noukod.com>
2019-02-21 18:29:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 const currentUser = "Clifford LOUIS";
 
-const welcomeMessage = `Welcome to Flatbook,  ${currentUser}!`
+const welcomeMessage = `Welcome to Flatbook,  ${currentUser}!`;
